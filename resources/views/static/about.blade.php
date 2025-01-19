1b853f4d892a202bce7e115f7ddea9f6f857886a
Maged Groups <magedyaseengroups@gmail.com>
2025-01-19 22:30:04 +0200

    Lec: 02-Routing - Basics

---
+++ resources/views/static/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+
+   @vite( 'resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="p-5 bg-green-300 text-green-900s">About us</h1>
+    <p class="p-5">TWET Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi vero eveniet vitae soluta pariatur accusantium blanditiis ab at, perspiciatis adipisci nesciunt quidem fuga cum modi alias quos voluptas repellendus maiores.</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,96 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('about', function () {
+//     return 'About us page';
+// });
+
+Route::get('about', function () {
+    return view('static/about');
+});
+
+
+// Route::get('contact-us', function () {
+//     echo '<h2>Contact us Page</h2>';
+// });
+
+Route::get('contact-us', function () {
+    return view('static.contacts');
+});
+
+
+Route::get('services', function () {
+    ?>
+    <h1 class="bg-green-100">Our services</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, dolore?</p>
+    <p>Eaque velit eius tempora dicta sit, architecto veniam. Obcaecati, asperiores!</p>
+    <p>Magni quo perferendis quis veniam nesciunt a eos dolores odio?</p>
+    <p>Iure vel qui nemo aspernatur ad vitae, omnis earum odio.</p>
+    <p>Officiis expedita at sequi voluptas voluptatum veritatis ex iusto adipisci.</p>
+    <?php
+});
+
+
+Route::get('employees', function () {
+    return 'Employees Page';
+});
+
+Route::get('employees/{id}', function ($id) {
+    return view('employees', ['id' => $id]);
+});
+
+
+Route::get('reports/{month}/{year}', function ($month, $year) {
+
+
+    // $name = 'Maged';
+
+    $data = compact('year', 'month');
+
+    // dd($data);
+
+
+    // return view('monthly-reports', ['month' => $month, 'year' => $year]);
+    return view('monthly-reports', $data);
+});
+
+
+
+Route::get('characters/{id?}', function ($id = 0) {
+
+    if ($id === 0)
+        return view('characters.all');
+
+    return view('characters.single', compact('id'));
+});
+
+// https://dummyjson.com/
+Route::get('users', function () {
+    // Communicate with database to get all users' data
+});
+
+
+Route::get('products', function () {
+    // Communicate with database to get all products
+});
+
+
+// (((((((((        Not for web routes      )))))))))
+// Route::get('products/{id}', function ($id) {
+//     // Communicate with database to get single product by id
+
+//     return response()->json([
+//         'id' => $id,
+//         'name' => 'Pink Shoes for girls',
+//         'price' => 1540.66,
+//         'photo' => 'https://www.imgs.com/pink/shoes/girl.png',
+//         'discount' => 0.1,
+//     ]);
+// });
+
+
